d502e3858e6f8be2bfbdc827b515e85989546413
chiru <chiru@chirus-MacBook-Air.local>
2025-02-21 18:04:35 +0530

    added project

---
+++ script/simpleStorage.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+
+contract DeployScript is Script{
+   function run() external returns(SimpleStorage){
+        vm.broadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.broadcast();
+        return simpleStorage;
+   }
+}
---
+++ test/simpleStorage.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+import {DeployScript} from "../script/simpleStorage.s.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract SimpleStorageTest is Test {
+    SimpleStorage public simpleStorage;
+    function setUp() external {
+        DeployScript deployer = new DeployScript();
+        simpleStorage = deployer.run();
+    }
+    function testStoreNumber() public {
+        uint256 expectedFavourateNumber = 777;
+        simpleStorage.store(expectedFavourateNumber);
+        assert(expectedFavourateNumber == simpleStorage.retrive());
+    }
+}
