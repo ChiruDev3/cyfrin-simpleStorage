957dc002ce3307eab8f4d33d05eb2f4458521ceb
chiru <chiru@chirus-MacBook-Air.local>
2025-02-22 10:06:13 +0530

    formated

--- src/simpleStorage.sol
+++ src/simpleStorage.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.8.28;
 
 contract SimpleStorage {
     uint256 myFavourateNumber;
+
     struct Person {
         uint256 favourateNumber;
         string name;
     }
+
     Person[] public listOfPeople;
-    mapping(string name => uint256 favourateNumber)
-        public nameToFavourateNumber;
+    mapping(string name => uint256 favourateNumber) public nameToFavourateNumber;
+
     function store(uint256 _favourteNumber) public {
         myFavourateNumber = _favourteNumber;
     }
+
     function addPerson(string memory _name, uint256 _favourateNumber) public {
-       listOfPeople.push(Person(_favourateNumber,_name));
-       nameToFavourateNumber[_name] = _favourateNumber;
+        listOfPeople.push(Person(_favourateNumber, _name));
+        nameToFavourateNumber[_name] = _favourateNumber;
     }
-    function retrive() public view returns(uint256){
+
+    function retrive() public view returns (uint256) {
         return myFavourateNumber;
     }
 }
